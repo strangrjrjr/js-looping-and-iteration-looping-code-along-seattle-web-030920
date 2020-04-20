3c9b7c27fea26627d4724368de2a73d342de2a13
Roger Robertson Jr <rjrjr@pm.me>
2020-04-19 19:03:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let result = []
+    for (let i = 0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return result;
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
